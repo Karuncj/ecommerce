38870815ec490f7d9a9e08549478531088714e5f
KARUN CHERY_JAMES <karunchery@gmail.com>
2024-08-30 21:26:26 +0530

    new file:   helpers/product-helpers.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/mongod/.coveralls.yml 	new file:   node_modules/mongod/.eslintrc.yml 	new file:   node_modules/mongod/.npmignore 	new file:   node_modules/mongod/.travis.yml 	new file:   node_modules/mongod/CHANGELOG.md 	new file:   node_modules/mongod/LICENSE.md 	new file:   node_modules/mongod/Mongod.js 	new file:   node_modules/mongod/README.md 	new file:   node_modules/mongod/package.json 	new file:   node_modules/promise-queue/.csscomb.json 	new file:   node_modules/promise-queue/.editorconfig 	new file:   node_modules/promise-queue/.jscs.json 	new file:   node_modules/promise-queue/.jshintignore 	new file:   node_modules/promise-queue/.jshintrc 	new file:   node_modules/promise-queue/.npmignore 	new file:   node_modules/promise-queue/.travis.yml 	new file:   node_modules/promise-queue/AUTHORS 	new file:   node_modules/promise-queue/CHANGELOG.md 	new file:   node_modules/promise-queue/CONTRIBUTION.md 	new file:   node_modules/promise-queue/LICENSE 	new file:   node_modules/promise-queue/Makefile 	new file:   node_modules/promise-queue/README.md 	new file:   node_modules/promise-queue/index.js 	new file:   node_modules/promise-queue/lib-cov/index.js 	new file:   node_modules/promise-queue/lib/index.js 	new file:   node_modules/promise-queue/package.json 	modified:   package-lock.json 	modified:   package.json 	modified:   routes/admin.js

--- configuration/connection.js
+++ configuration/connection.js
@@ -5,7 +5,7 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:127.0.0.1.27017'
+    const url='mongodb://localhost:27017'
     const dbname='shopping'
 
     mongoClient.connect(url,(err,data)=>{
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var productHelper=require('../helpers/product-helpers')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   let products=
@@ -34,5 +34,9 @@ router.get('/add-product',(req,res)=>{
 router.post('/add-product',(req,res)=>{
   console.log(req.body)
   console.log(req.files.image)
+
+  productHelper.addProduct((req.body),(result)=>{
+    res.render("admin/add-product")
+  })
 })
 module.exports = router;
