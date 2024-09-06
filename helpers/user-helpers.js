b19555452d98fb9084b79cf4739163bc69f29b3b
KARUN CHERY_JAMES <karunchery@gmail.com>
2024-09-06 21:51:38 +0530

    modified:   views/admin/edit-product.hbs

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,37 +1,62 @@
 const { resolve } = require('promise')
-var collection=require('../configuration/collections')
-var db=require('../configuration/connection')
-const bcrypt=require('bcrypt')
+var collection = require('../configuration/collections')
+var db = require('../configuration/connection')
+const bcrypt = require('bcrypt')
 const { response } = require('express')
-module.exports={
-    doSignup:(userData)=>{
-        return new Promise(async(resolve,reject)=>{
+var { ObjectId } = require('mongodb')
+module.exports = {
+    doSignup: (userData) => {
+        return new Promise(async (resolve, reject) => {
             delete userData.confirmPassword;
-        userData.password=await bcrypt.hash(userData.password,10)
-        db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data)=>{
-            resolve(data.insertedId)
+            userData.password = await bcrypt.hash(userData.password, 10)
+            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data) => {
+                resolve(data.insertedId)
+            })
         })
-    })
     },
-    doLogin:(userData)=>{
-        return new Promise(async(resolve,reject)=>{
-            let user=await db.get().collection(collection.USER_COLLECTION).findOne({email:userData.email})
-            if(user){
-                bcrypt.compare(userData.password,user.password).then((status)=>{
-                    if(status){
+    doLogin: (userData) => {
+        return new Promise(async (resolve, reject) => {
+            let user = await db.get().collection(collection.USER_COLLECTION).findOne({ email: userData.email })
+            if (user) {
+                bcrypt.compare(userData.password, user.password).then((status) => {
+                    if (status) {
                         console.log("login sucess")
-                        response.user=user
-                        response.status=true
+                        response.user = user
+                        response.status = true
                         resolve(response)
                     }
-                    else{
+                    else {
                         console.log("Login failed")
-                        resolve({status:false})
+                        resolve({ status: false })
                     }
                 })
             }
-            else{
+            else {
                 console.log("User not exist")
+            }
+        })
+    },
+    addToCart: (prodId, userId) => {
+        return new Promise(async (resolve, reject) => {
+            let userCart = await db.get().collection(collection.CART_COLLECTION).findOne({ user: ObjectId.createFromHexString(userId) })
+            if (userCart) {
+                db.get().collection(collection.CART_COLLECTION)
+                    .updateOne(({ user: ObjectId.createFromHexString(userId) }),
+                        {
+                            $push: {products: ObjectId.createFromHexString(prodId)}
+
+                        }
+                    ).then((response) => {
+                        resolve()
+                    })
+            } else {
+                let cartObj = {
+                    user: ObjectId.createFromHexString(userId),
+                    products: [ObjectId.createFromHexString(prodId)]
+                }
+                db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response) => {
+                    resolve()
+                })
             }
         })
     }
--- routes/user.js
+++ routes/user.js
@@ -1,70 +1,76 @@
 var express = require('express');
 var router = express.Router();
 var productHelper = require('.././helpers/product-helpers')
-const userHelpers=require('../helpers/user-helpers')
+const userHelpers = require('../helpers/user-helpers')
 
-const verifyLogin=(req,res,next)=>{
-  if(req.session.loggedIn){
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
     next()
   }
-  else{
+  else {
     res.redirect('/login')
   }
 }
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let user=req.session.user
+  let user = req.session.user
   console.log(user)
   productHelper.getAllProducts().then((products) => {
-    res.render('user/view-products', { products,user });
+    res.render('user/view-products', { products, user });
   })
-}); 
+});
 
-router.get('/login',(req,res)=>{
-  if(req.session.loggedIn){
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
     res.redirect('/')
-  }else{
-  res.render('user/login',{"loginErr":req.session.loginErr})
-  req.session.loginErr=false
+  } else {
+    res.render('user/login', { "loginErr": req.session.loginErr })
+    req.session.loginErr = false
   }
 })
 
-router.get('/signup',(req,res)=>{
+router.get('/signup', (req, res) => {
   res.render('user/signup')
 })
 
-router.post('/signup',(req,res)=>{
-  userHelpers.doSignup(req.body).then((response)=>{
+router.post('/signup', (req, res) => {
+  userHelpers.doSignup(req.body).then((response) => {
     console.log(response)
+    req.session.loggedIn = true
+    req.session.user = response
     res.render('/')
   })
 })
 
 
-router.post('/login',(req,res)=>{
-  userHelpers.doLogin(req.body).then((response)=>{
-    if(response.status){
-      req.session.loggedIn=true
-      req.session.user=response.user
-    res.redirect('/')
-  }
-  else{
-    req.session.loginErr="Invalid Username or password"
-    res.redirect('login')
-  }
+router.post('/login', (req, res) => {
+  userHelpers.doLogin(req.body).then((response) => {
+    if (response.status) {
+      req.session.loggedIn = true
+      req.session.user = response.user
+      res.redirect('/')
+    }
+    else {
+      req.session.loginErr = "Invalid Username or password"
+      res.redirect('login')
+    }
   })
 })
 
-router.get('/logout',(req,res)=>{
+router.get('/logout', (req, res) => {
   req.session.destroy()
   redirect('/login')
 })
 
-router.get('/cart',verifyLogin,(req,res)=>{
-  
+router.get('/cart', verifyLogin, (req, res) => {
+
   res.render('user/cart')
 })
 
-
+router.get('/add-to-cart/:id', verifyLogin, (req, res) => {
+  userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
+    res.redirect('/')
+  })
+})
 module.exports = router;
